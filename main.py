2ca948c05c6d8c1aa73911dc9a0b5306e85c8efb
developermano <87554427+developermano@users.noreply.github.com>
2022-01-01 16:56:03 +0000

    download video function is added

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import os
-from telegram import Update
+from telegram import Update,Bot
 from telegram.ext import Updater, CommandHandler, CallbackContext
+from pytube import Channel
 
+bot=Bot(os.environ['token'])
 
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Hi! .welcome to @Eritybot . run /channel command to upload all videos to this chat.')
   
 
 def channel(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(context.args[0])
+    c = Channel(context.args[0])
+    #for url in c.video_urls:
+    #  print(url)
+    for video in c.videos:
+      fi=video.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+      print(fi)
+      bot.send_video(update.effective_message.chat_id,open(fi, 'rb'))
+      os.remove(fi)
 
 def main() -> None:
     """Run the bot."""
